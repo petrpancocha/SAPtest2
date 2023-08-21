f044f8b69deb1889747f788fc49aac2f9939ffc1
petrpancocha <petr.pancocha@seznam.cz>
2023-08-21 10:59:55 +0200

    Code simplification - zones/roads/complaints are kept as attributes of Main class -> there is no need to pass lists over methods' params again and again

--- src/main/java/com/petrpancocha/saptest/Main.java
+++ src/main/java/com/petrpancocha/saptest/Main.java
@@ -9,33 +9,36 @@ public class Main {
     private static final int[][] ROADS = {{1, 2, 5}, {2, 3, 10}, {3, 4, 20}, {1, 4, 1}};
     private static final int[][] COMPLAINTS = {{2, 65}, {3, 15}};
 
+    // zones/roads/complaints as model objects
+    private List<Zone> zones;
+    private List<Road> roads;
+    private List<Complaint> complaints;
+
     //-----------------------------------------------------------------------------------
     // Methods for preparation of model objects from input "array data"
-    private List<Zone> createZones() {
-        List<Zone> zones = new ArrayList<>();
+    private void createZones() {
+        this.zones = new ArrayList<>();
 
         for (int i = 1; i <= NUM_ZONES; i++) {
             zones.add(new Zone(i));
         }
-
-        return zones;
     }
 
-    private Zone getZoneById(List<Zone> zones, int zoneId) {
+    private Zone getZoneById(int zoneId) {
         return zones.stream()
                 .filter(z -> z.getId() == zoneId)
                 .findFirst()
                 .orElse(null);
     }
 
-    private Zone getZoneByGraphMappingId(List<Zone> zones, int graphMappingId) {
+    private Zone getZoneByGraphMappingId(int graphMappingId) {
         return zones.stream()
                 .filter(z -> z.getGraphMappingId() == graphMappingId)
                 .findFirst()
                 .orElse(null);
     }
 
-    private Road getRoadByZones(List<Road> roads, int fromZoneId, int toZoneId) {
+    private Road getRoadByZones(int fromZoneId, int toZoneId) {
         return roads.stream()
                 .filter(r -> (r.getZoneA().getId() == fromZoneId && r.getZoneB().getId() == toZoneId)
                         || (r.getZoneA().getId() == toZoneId && r.getZoneB().getId() == fromZoneId))
@@ -43,32 +46,28 @@ public class Main {
                 .orElse(null);
     }
 
-    private List<Road> createRoads(List<Zone> zones) {
-        List<Road> roads = new ArrayList<>();
+    private void createRoads() {
+        this.roads = new ArrayList<>();
 
         for (int[] road : ROADS) {
             roads.add(new Road(
-                    getZoneById(zones, road[0]),
-                    getZoneById(zones, road[1]),
+                    getZoneById(road[0]),
+                    getZoneById(road[1]),
                     road[2]));
         }
-
-        return roads;
     }
 
-    private List<Complaint> createComplaints(List<Zone> zones) {
-        List<Complaint> complaints = new ArrayList<>();
+    private void createComplaints() {
+        this.complaints = new ArrayList<>();
 
         for (int[] complaint : COMPLAINTS) {
             complaints.add(new Complaint(
-                    getZoneById(zones, complaint[0]),
+                    getZoneById(complaint[0]),
                     complaint[1]));
         }
-
-        return complaints;
     }
 
-    private Graph createGraph(List<Road> roads) {
+    private Graph createGraph() {
         Graph graph = new Graph(NUM_ZONES);
 
         for (Road road : roads) {
@@ -79,20 +78,20 @@ public class Main {
         return graph;
     }
 
-    private List<Path> convert2Paths(List<List<Integer>> graphPaths, List<Zone> zones, List<Road> roads) {
+    private List<Path> convert2Paths(List<List<Integer>> graphPaths) {
         List<Path> paths = new ArrayList<>();
 
         for (List<Integer> graphPath : graphPaths) {
-            Zone from = getZoneByGraphMappingId(zones, graphPath.get(0));
-            Zone to = getZoneByGraphMappingId(zones, graphPath.get(graphPath.size() - 1));
+            Zone from = getZoneByGraphMappingId(graphPath.get(0));
+            Zone to = getZoneByGraphMappingId(graphPath.get(graphPath.size() - 1));
 
             Path path = new Path(from, to);
 
             for (int i = 0; i < graphPath.size() - 1; i++) {
-                int idZoneA = getZoneByGraphMappingId(zones, graphPath.get(i)).getId();
-                int idZoneB = getZoneByGraphMappingId(zones, graphPath.get(i + 1)).getId();
+                int idZoneA = getZoneByGraphMappingId(graphPath.get(i)).getId();
+                int idZoneB = getZoneByGraphMappingId(graphPath.get(i + 1)).getId();
 
-                Road road = getRoadByZones(roads, idZoneA, idZoneB);
+                Road road = getRoadByZones(idZoneA, idZoneB);
                 path.addRoad(road);
             }
 
@@ -105,12 +104,12 @@ public class Main {
     //-----------------------------------------------------------------------------------
     // Methods for business logic
 
-    private void process(Graph graph, List<Zone> zones, List<Road> roads, List<Complaint> complaints) {
-        Zone zoneHQ = getZoneById(zones, 1);
+    private void process(Graph graph) {
+        Zone zoneHQ = getZoneById(1);
         for (Complaint complaint : complaints) {
             System.out.println("All paths from " + zoneHQ + " to " + complaint.getZone() + " to fix an issue");
             List<List<Integer>> graphPaths = graph.findAllPaths(zoneHQ.getGraphMappingId(), complaint.getZone().getGraphMappingId());
-            List<Path> paths = convert2Paths(graphPaths, zones, roads);
+            List<Path> paths = convert2Paths(graphPaths);
 
             int minimumTravelTime = -1;
             Path pathWithMinimumTravelTime = null;
@@ -142,13 +141,14 @@ public class Main {
         Main main = new Main();
 
         // prepare model objects
-        List<Zone> zones = main.createZones();
-        List<Road> roads = main.createRoads(zones);
-        List<Complaint> complaints = main.createComplaints(zones);
+        main.createZones();
+        main.createRoads();
+        main.createComplaints();
 
-        Graph graph = main.createGraph(roads); // create graph representation of roads
+        // create graph representation of roads
+        Graph graph = main.createGraph();
 
         // execute business logic
-        main.process(graph, zones, roads, complaints);
+        main.process(graph);
     }
 }
